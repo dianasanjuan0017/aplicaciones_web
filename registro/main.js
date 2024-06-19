c56d40dcd95ab16d71f5ec957390cc96296d7c67
dianasanjuan0017 <20230019@uthh.edu.mx>
2024-06-19 08:33:16 -1100

    deleted:    registro/formulario.html 	deleted:    registro/main.js 	deleted:    registro/style.css

--- registro/main.js
+++
@@ -1,9 +0,0 @@
-function confirma(){
-    pa=document.getElementById("pass").value
-    pb=document.getElementById("pass2").value
-
-    if(pa!=pb){
-        alert("Los passwords no coinciden")
-        return
-    }
-}
